0656c9d894acfff6649abb8df2a0070cc01e9078
Vijaysrinivas Rajagopal <vijaythinks@gmail.com>
2020-04-20 15:12:16 -0400

    temp new project

--- android/p2pkit-quickstart-android/app/src/main/java/ch/uepaa/quickstart/MainActivity.java
+++ android/p2pkit-quickstart-android/app/src/main/java/ch/uepaa/quickstart/MainActivity.java
@@ -37,7 +37,7 @@ import ch.uepaa.quickstart.utils.Logger;
 
 public class MainActivity extends AppCompatActivity implements ConsoleFragment.ConsoleListener, ColorPickerFragment.ColorPickerListener {
 
-    private static final String APP_KEY = "<CHANGE ME>";
+    private static final String APP_KEY = "";
 
     public void enableP2PKit() {
         try {
